4645ee780c6e9be8549622347434cf1f681f7ce4
Barreiro14 <g.barreiro140@gmail.com>
2021-12-10 17:13:17 -0400

    fix solver

---
+++ syssolver.py
@@ -0,0 +1,15 @@
+import numpy as np
+from values import Values
+from xysums import xValues, xyValues
+
+def order(n): #el orden es el order del polinomio de Taylor a evaluar
+    w = np.zeros((n + 1, n + 1))
+    i = 0
+    j = 0
+    while i < len(w):
+        while j < len(w[0]):
+            print([i, j])
+            j = j + 1
+        i = i + 1
+
+order(2)
